42bb2ed82406fa1c87d0330a4be93018c654646d
Obi <obialfred@gmail.com>
2019-09-26 16:53:58 -0700

    first commit

---
+++ Hangman.java
@@ -0,0 +1,27 @@
+import java.Util.Scanner;
+
+import com.sun.tools.javac.code.Attribute.Array;
+
+class Hangman {
+    Scanner ui = new Scanner(System.in);
+    String phrase[];
+    String matchingChar[];
+    int wrong = 0;
+    int right = 0;
+   for (i=0; i<ui.length; i++)
+    for(j=0; j<phrase.length; j++){
+        if (ui[i] == phrase[j]){
+
+        }
+    }
+
+
+    public static void main(String[]args){
+        System.out.println("Welcome to Hangman!");
+        // hidden phrase gets called here
+        System.out.println("Please guess the phrase:");
+        //user input goes here
+    }
+    
+
+}
